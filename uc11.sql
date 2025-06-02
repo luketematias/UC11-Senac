445970a2c224780acee5825c1b788adc8ecafbda
Lucas Matias <luccasmatias01@gmail.com>
2025-06-01 22:17:36 -0300

    Atualizado o script do banco de dados

--- uc11.sql
+++ uc11.sql
@@ -20,7 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `uc11`
 --
-
+drop database if exists uc11;
+create database uc11;
+use uc11;
 -- --------------------------------------------------------
 
 --
